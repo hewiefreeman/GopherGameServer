8d209e46fabd8ba9b8ede86f963e557235ae21d5
hewiefreeman <dominiquedebergue@gmail.com>
2019-01-25 22:28:23 -0800

    Adding more useful macros

--- macros.go
+++ macros.go
@@ -34,13 +34,16 @@ func handleMacro(macro string) bool {
 		macroDeleteRoom(macro)
 	} else if len(macro) >= 9 && macro[0:7] == "newroom" {
 		macroNewRoom(macro)
+	} else if len(macro) >= 9 && macro[0:7] == "getuser" {
+		macroGetUser(macro)
+	} else if len(macro) >= 9 && macro[0:7] == "getroom" {
+		macroGetRoom(macro)
 	} else if len(macro) >= 6 && macro[0:4] == "kick" {
 		macroKick(macro)
 	}
 	return false
 }
 
-// KICK A USER
 func macroKick(macro string) {
 	userName := macro[5:]
 	user, userErr := users.Get(userName)
@@ -92,4 +95,54 @@ func macroDeleteRoom(macro string) {
 		return
 	}
 	fmt.Println("Deleted room '" + s[1] + "'")
+}
+
+func macroGetUser(macro string) {
+	s := strings.Split(macro, " ")
+	if len(s) != 2 {
+		fmt.Println("getuser expects 1 parameter (name string)")
+		return
+	}
+
+	user, userErr := users.Get(s[1])
+	if userErr != nil {
+		fmt.Println(userErr)
+		return
+	}
+
+	fmt.Println("-- User '"+s[1]+"' --")
+	fmt.Println("Status:", user.Status())
+	fmt.Println("Guest:", user.IsGuest())
+	fmt.Println("Connections:")
+	conns := user.ConnectionIDs()
+	for i := 0; i < len(conns); i++ {
+		fmt.Println("    [ ID: '"+conns[i]+"', Room: '"+user.RoomIn(conns[i]).Name()+"', Vars:", user.GetVariables(nil, conns[i]), "]")
+	}
+	fmt.Println("Friends:", user.Friends())
+	fmt.Println("Database ID:", user.DatabaseID())
+}
+
+func macroGetRoom(macro string) {
+	s := strings.Split(macro, " ")
+	if len(s) != 2 {
+		fmt.Println("getroom expects 1 parameter (name string)")
+		return
+	}
+
+	room, roomErr := rooms.Get(s[1])
+	if roomErr != nil {
+		fmt.Println(roomErr)
+		return
+	}
+
+	invList, _ := room.InviteList()
+	usrMap, _ := room.GetUserMap()
+
+	fmt.Println("-- Room '"+s[1]+"' --")
+	fmt.Println("Type:", room.Type())
+	fmt.Println("Private:", room.IsPrivate())
+	fmt.Println("Owner:", room.Owner())
+	fmt.Println("Max Users:", room.MaxUsers())
+	fmt.Println("Users:", "("+strconv.Itoa(room.NumUsers())+")", usrMap)
+	fmt.Println("Invite List:", invList)
 }
