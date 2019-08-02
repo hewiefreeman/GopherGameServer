cff4ffef78ba529d88a871dfd503257656765157
hewiefreeman <dominiquedebergue@gmail.com>
2019-08-01 20:53:37 -0700

    Creating tests & project site

---
+++ startandstop_test.go
@@ -0,0 +1,14 @@
+package gopher
+
+import (
+	"testing"
+	"time"
+)
+
+func TestStartAndStop(t *testing.T) {
+	go Start(nil)
+	time.Sleep(time.Second*2)
+	if sdErr := ShutDown(); sdErr != nil {
+		t.Errorf(sdErr.Error())
+	}
+}
