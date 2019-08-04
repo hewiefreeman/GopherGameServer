c9d40659820f4e6fb056ce0067b1988db8fefa29
hewiefreeman <dominiquedebergue@gmail.com>
2019-08-03 17:18:18 -0700

    Adding site responsiveness

--- docs/js/init.js
+++ docs/js/init.js
@@ -1,10 +1,14 @@
 window.addEventListener("load", init);
 var	download_nav,
-	download_btn;
+	download_btn,
+	logo,
+	title;
 
 function init() {
 	download_nav = document.getElementById("downloadNav");
 	download_btn = document.getElementById("downloadBtn");
+	logo = document.getElementById("logo");
+	title = document.getElementById("title");
 
 	window.addEventListener("resize", resized);
 	//
--- docs/js/resize.js
+++ docs/js/resize.js
@@ -1,7 +1,10 @@
-var download_btn_hidden = false;
-var download_btn_switch = 520;
+var	download_btn_hidden = false,
+	download_btn_switch = 520,
+	logo_resize = false,
+	logo_switch = 350;
 
 function resized(e) {
+	// Resize and replace download button
 	if (document.documentElement.clientWidth <= download_btn_switch && !download_btn_hidden) {
 		download_nav.style.display = "inline-block";
 		download_btn.style.display = "none";
@@ -12,6 +15,19 @@ function resized(e) {
 		download_btn.style.display = "block";
 
 		download_btn_hidden = false;
+	}
+
+	// Resize logo
+	if (document.documentElement.clientWidth <= download_btn_switch) {
+		logo_resize = true;
+		logo.style.width = (document.documentElement.clientWidth/2)+"px";
+		logo.style.height = (document.documentElement.clientWidth/2)+"px";
+		title.style.left = ((document.documentElement.clientWidth/2)-30)+"px";
+	} else if (document.documentElement.clientWidth > download_btn_switch && logo_resize) {
+		logo.style.width = "230px";
+		logo.style.height = "230px";
+		title.style.left = "220px";
+		logo_resize = false;
 	}
 }
 
